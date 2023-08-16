441dfcccbc51e10987dc92267f68659bb56be496
unknown <ritujivtode13@gmail.com>
2023-08-16 17:43:07 +0530

    ignore node-modules

--- Backend/utils/mysql.js
+++ Backend/utils/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'medidekdb.cxajit2uhdcv.us-east-1.rds.amazonaws.com',
-  user: 'root',
-  password: 'jivtodeRitu',
-  database: 'medidek_schema'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 connection.connect(function(err) {
